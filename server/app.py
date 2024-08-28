d297480bd481b2cb4967b0186062bc60ac24351d
Lucy Kariuki <kariuki.lucy@strathmore.edu>
2024-08-28 21:04:24 +0300

    Created an API flask for a react frontend application

--- server/app.py
+++ server/app.py
@@ -14,13 +14,66 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.order_by('created_at').all()
 
-@app.route('/messages/<int:id>')
+        response = make_response(
+            jsonify([message.to_dict() for message in messages]),
+            200,
+        )
+    elif request.method == 'POST':
+        data = request.get_json()
+        #structure of our message
+        message = Message(
+            body= data['body'],
+            username= data['username']
+        )
+
+        db.session.add(message)
+        db.session.commit()
+
+        response = make_response(
+            jsonify(message.to_dict()),
+            201,
+        )
+
+    return response
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    # Retrieve the message from the db
+    message = Message.query.filter_by(id=id).first()
+
+    if not message:
+        return make_response(jsonify({"error": "Message not found"}), 404)
+
+    # Handle the PATCH method
+    if request.method == 'PATCH':
+        data = request.get_json()
+        # Loop to iterate over the key values
+        for attr in data:
+            setattr(message, attr, data[attr])
+
+        # Add message to the db
+        db.session.add(message)
+        db.session.commit() 
+
+        response = make_response(
+            jsonify(message.to_dict()),
+            200,
+        )
+
+    elif request.method == 'DELETE':
+        # Ensure we delete it in the db
+        db.session.delete(message)
+        db.session.commit()
+
+        # Return a response indicating successful deletion
+        response = make_response('', 204)
+
+    return response
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5000)
